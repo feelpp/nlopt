c8a927133ec7359592f2e05c49eae2de5f671a07
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2014-10-18 07:58:16 +0200

    up feelpp/feelpp@466  - problem at link time fixed (undefined symbol)

--- bobyqa/bobyqa.h
+++ bobyqa/bobyqa.h
@@ -4,10 +4,11 @@
 #include "nlopt-util.h"
 #include "nlopt.h"
 
-extern nlopt_result bobyqa(int n, int npt, double *x, 
+extern "C" {
+nlopt_result bobyqa(int n, int npt, double *x, 
 			   const double *lb, const double *ub,
 			   const double *dx, 
 			   nlopt_stopping *stop, double *minf,
 			   nlopt_func f, void *f_data);
-
+}
 #endif /* BOBYQA_H */
