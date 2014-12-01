e9e1b0dbd76ee010706bd31df8ef442986239fdf
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2014-12-01 08:36:29 +0100

    fix compilation on yosemite(at least)

--- bobyqa/bobyqa.h
+++ bobyqa/bobyqa.h
@@ -3,11 +3,14 @@
 
 #include "nlopt-util.h"
 #include "nlopt.h"
+ 
+extern "C" 
+{
 
-extern nlopt_result bobyqa(int n, int npt, double *x, 
-			   const double *lb, const double *ub,
-			   const double *dx, 
-			   nlopt_stopping *stop, double *minf,
-			   nlopt_func f, void *f_data);
-
+nlopt_result bobyqa(int n, int npt, double *x, 
+                        const double *lb, const double *ub,
+                        const double *dx, 
+                        nlopt_stopping *stop, double *minf,
+                        nlopt_func f, void *f_data);
+}
 #endif /* BOBYQA_H */
