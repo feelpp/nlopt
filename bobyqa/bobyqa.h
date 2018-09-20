54cdb326e591dd68e8055ee6f0a9530f76b9ad86
Vincent Chabannes <vincent.chabannes@feelpp.org>
2018-09-20 09:34:26 +0200

    revert (not need to patch nlopt)

--- bobyqa/bobyqa.h
+++ bobyqa/bobyqa.h
@@ -4,18 +4,10 @@
 #include "nlopt-util.h"
 #include "nlopt.h"
 
-#if defined(__cplusplus)
-extern "C" 
-{
-#else
-  extern
-#endif
-nlopt_result bobyqa(int n, int npt, double *x, 
-                        const double *lb, const double *ub,
-                        const double *dx, 
-                        nlopt_stopping *stop, double *minf,
-                        nlopt_func f, void *f_data);
-#if defined(__cplusplus)  
-}
-#endif
+extern nlopt_result bobyqa(int n, int npt, double *x,
+                           const double *lb, const double *ub,
+                           const double *dx,
+                           nlopt_stopping *stop, double *minf,
+                           nlopt_func f, void *f_data);
+
 #endif /* BOBYQA_H */
